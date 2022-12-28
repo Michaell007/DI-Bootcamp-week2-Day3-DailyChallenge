c6ad678fe16bdd210e0783603ab513615a301542
MichaelYaps <michaelakichi@gmail.com>
2022-12-28 23:48:07 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let line = "";
+for (let i = 0; i <= 3; i++) {
+    line += "*";
+    console.log(line);
+    for (let j = 0; j <= 3; j++) {
+        line += "*";
+        console.log(line);
+    }
+}
